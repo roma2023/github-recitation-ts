e6916f223d4de9a18787c6350f46585abbf896ce
roma2023 <romasaparhan19@gmail.com>
2024-08-29 17:53:09 +0300

    update fibRoute.ts

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,9 +1,9 @@
 // Endpoint for querying the fibonacci numbers
 
-const fibonacci = require("./fib");
+import fibonacci from './fib';
 
 export default (req, res) => {
-  const { num } = req.params;
+  const num = req.params;
 
   const fibN = fibonacci(parseInt(num));
   let result = `fibonacci(${num}) is ${fibN}`;
